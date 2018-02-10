2924a99253e0823eedac438912a79f52168e3ad5
Joseph OHagan <josephaohagan@gmail.com>
2018-02-10 20:07:50 +0000

    Added ability to spawn copies of objects via script / button

---
+++ SVRA/Assets/Scripts/SVRA/SampleInteractions/SVRA_CopyObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SVRA_CopyObject : MonoBehaviour {
+
+    private bool toggleSpawn = false;
+
+    public GameObject copyObject;
+    public Transform spawnPoint;
+
+    public void CopyObject()
+    {
+        GameObject newObject = GameObject.Instantiate(copyObject);
+        newObject.transform.position = spawnPoint.position;
+        newObject.transform.localScale = copyObject.transform.lossyScale;        
+    }
+}
