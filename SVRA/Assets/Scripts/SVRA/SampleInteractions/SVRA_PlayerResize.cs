11c259167ca1bf5a92de05f2c2153529070139e7
Joseph OHagan <josephaohagan@gmail.com>
2018-02-10 20:08:50 +0000

    Added ability to scale player (1 thing yet to test)

---
+++ SVRA/Assets/Scripts/SVRA/SampleInteractions/SVRA_PlayerResize.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SVRA_PlayerResize : MonoBehaviour {
+
+    private Vector3 originalScale;
+    private SteamVR_PlayArea playArea;
+
+    [Tooltip("Should the player start default or resized")]
+    public bool sizeToggle = false;
+
+    [Tooltip("Vector size of the world to rescale to")]
+    public Vector3 toggleScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+    private void Awake()
+    {
+        playArea = GetComponentInParent<SteamVR_PlayArea>();
+        originalScale = playArea.transform.localScale;
+    }
+
+    void Start () {       
+        if (sizeToggle)
+        {
+            ResizePlayArea();
+        }
+
+        sizeToggle = !sizeToggle;
+    }
+	
+	/* Resize the play area to the specified value */
+	public void ResizePlayArea () {
+        playArea.transform.localScale = sizeToggle ? toggleScale : originalScale;
+
+    }
+    
+}
