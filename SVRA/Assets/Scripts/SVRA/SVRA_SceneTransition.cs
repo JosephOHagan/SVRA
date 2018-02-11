e4cb69525177e781c08bf4d46ad16b66ce0ac7dd
Joseph OHagan <josephaohagan@gmail.com>
2018-02-11 13:55:26 +0000

    Minor change and test

--- SVRA/Assets/Scripts/SVRA/SVRA_SceneTransition.cs
+++ SVRA/Assets/Scripts/SVRA/SVRA_SceneTransition.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class SVRA_SceneTransition : MonoBehaviour {
 
-    private SteamVR_LoadLevel SteamVR_LoadLevel;
-
     public string levelName;
 
     public void TransitionLevel()
     {
         SteamVR_LoadLevel.Begin(levelName);
     }
-
 }
