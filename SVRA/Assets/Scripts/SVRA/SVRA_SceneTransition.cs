973aee7b1bd114312640293e3a4d89cd34e8c88c
Joseph OHagan <josephaohagan@gmail.com>
2018-02-11 13:49:47 +0000

    Added simple scene transition script and example button

---
+++ SVRA/Assets/Scripts/SVRA/SVRA_SceneTransition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SVRA_SceneTransition : MonoBehaviour {
+
+    private SteamVR_LoadLevel SteamVR_LoadLevel;
+
+    public string levelName;
+
+    public void TransitionLevel()
+    {
+        SteamVR_LoadLevel.Begin(levelName);
+    }
+
+}
