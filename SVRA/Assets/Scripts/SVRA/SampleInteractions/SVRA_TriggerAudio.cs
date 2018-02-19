27d70c41521a753cf8c090674e7e0405149ae1fd
Joseph OHagan <josephaohagan@gmail.com>
2018-02-19 17:34:51 +0000

    First version of audio on trigger for event system

---
+++ SVRA/Assets/Scripts/SVRA/SampleInteractions/SVRA_TriggerAudio.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SVRA_TriggerAudio : MonoBehaviour
+{
+    AudioSource audio;
+    public bool pauseToggle = true;
+
+    private bool audioPlaying = false;
+    private bool audioPaused = false;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void TriggerAudio()
+    {
+        if (audioPlaying && pauseToggle)
+        {
+            Debug.Log("Audio Paused");
+
+            audio.Pause();
+            audioPlaying = false;
+        }
+        else if (audioPaused && pauseToggle)
+        {
+            Debug.Log("Audio Unpaused");
+
+            audio.UnPause();
+            audioPlaying = true;
+            audioPaused = false;
+        }    
+        else        
+        {
+            Debug.Log("Audio Playing");
+
+            audio.Play();
+            audioPlaying = true;
+        }
+    }
+}
