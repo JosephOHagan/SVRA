a25b6eae052fa2bafb472c0e8aef3b4a4376f6fb
Joseph OHagan <josephaohagan@gmail.com>
2018-02-20 16:45:28 +0000

    Delayed start extension to the audio trigger script

--- SVRA/Assets/Scripts/SVRA/SampleInteractions/SVRA_TriggerAudio.cs
+++ SVRA/Assets/Scripts/SVRA/SampleInteractions/SVRA_TriggerAudio.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class SVRA_TriggerAudio : MonoBehaviour
 {
+    [System.Serializable]
+    public class DelayedStart
+    {
+        [Tooltip("Enable delayed start")]
+        public bool enable = false;
+
+        [Tooltip("The time in seconds to delay the start of the audio by")]
+        public float seconds = 0.0f;
+    }
+
+    [Tooltip("Allow the audio to be paused")]
+    public bool pauseEnabled = true;
+
+    [Tooltip("Setup the audio to start with a delayed start")]
+    public DelayedStart delayedStart;
+
     AudioSource audio;
-    public bool pauseToggle = true;
 
     private bool audioPlaying = false;
     private bool audioPaused = false;
@@ -17,26 +32,53 @@ public class SVRA_TriggerAudio : MonoBehaviour
     }
 
     public void TriggerAudio()
-    {
-        if (audioPlaying && pauseToggle)
+    {        
+        if (delayedStart.enable)
         {
-            Debug.Log("Audio Paused");
+            DelayedAudioStart();            
+        }
+        else
+        {
+            NonDelayedAudioStart();
+        }
+    }
 
+    /* Play the audio with the specified delayed starting time */
+    void DelayedAudioStart()
+    {
+        if (audioPlaying && pauseEnabled)
+        {
             audio.Pause();
             audioPlaying = false;
         }
-        else if (audioPaused && pauseToggle)
+        else if (audioPaused && pauseEnabled)
         {
-            Debug.Log("Audio Unpaused");
-
             audio.UnPause();
             audioPlaying = true;
             audioPaused = false;
-        }    
-        else        
+        }
+        else
         {
-            Debug.Log("Audio Playing");
+            audio.PlayDelayed(delayedStart.seconds);
+            audioPlaying = true;
+        }
+    }
 
+    void NonDelayedAudioStart()
+    {
+        if (audioPlaying && pauseEnabled)
+        {
+            audio.Pause();
+            audioPlaying = false;
+        }
+        else if (audioPaused && pauseEnabled)
+        {
+            audio.UnPause();
+            audioPlaying = true;
+            audioPaused = false;
+        }
+        else
+        {
             audio.Play();
             audioPlaying = true;
         }
