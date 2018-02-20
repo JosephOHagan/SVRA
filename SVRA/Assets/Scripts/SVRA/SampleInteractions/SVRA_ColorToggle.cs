405bb3b8fab150aa767d0924feabba91e722b880
Joseph OHagan <josephaohagan@gmail.com>
2018-02-20 16:10:40 +0000

    New color and material queue system (TODO: Rename Script To Something Else)

--- SVRA/Assets/Scripts/SVRA/SampleInteractions/SVRA_ColorToggle.cs
+++ SVRA/Assets/Scripts/SVRA/SampleInteractions/SVRA_ColorToggle.cs
@@ -4,24 +4,69 @@ using UnityEngine;
 
 public class SVRA_ColorToggle : MonoBehaviour
 {
-    private bool toggle = false;
+    [Tooltip("Make the change of color permanent")]
+    public bool permanentChange = false;
+
+    [Tooltip("The list of colors to change between")]
+    public Color[] colorList;
+
+    private bool singleColorToggle = true;
     private Color originalColor;
-
-    [Tooltip("The toggle color")]
-    public Color toggleColor;
-
-    // TODO : Add the ability to cycle through colors (color queue)
-    // public Color[] test;
-
+    private Queue colorQueue = new Queue();
+        
     private void Awake()
     {
         originalColor = GetComponent<Renderer>().material.color;
+
+        for (int i = 0; i < colorList.Length; i++)
+        {
+            colorQueue.Enqueue(colorList[i]);
+        }
     }
 
-    public void ToggleColor()
+    /* Change the color of the attached to object */
+    public void ChangeColor()
     {
-        GetComponent<Renderer>().material.color = (toggle ? toggleColor : originalColor);
-        toggle = !toggle;
+        if (colorList.Length == 1)
+        {
+            SingleColorChange();
+        }
+        else
+        {
+            ColorQueueChange();
+        }
     }
 
+    /* Toggle between the original and specified color */
+    void SingleColorChange()
+    {
+        if (permanentChange)
+        {
+            GetComponent<Renderer>().material.color = colorList[0];
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = (singleColorToggle ? colorList[0] : originalColor);
+            singleColorToggle = !singleColorToggle;
+        }        
+    }
+
+    /* Cycle through a queue of colors back to the original color */
+    void ColorQueueChange()
+    {
+        if (permanentChange)
+        {
+            if (colorQueue.Count > 0)
+            {
+                GetComponent<Renderer>().material.color = (Color)colorQueue.Dequeue();
+            }
+
+            return;            
+        }
+        else
+        {
+            colorQueue.Enqueue(GetComponent<Renderer>().material.color);
+            GetComponent<Renderer>().material.color = (Color)colorQueue.Dequeue();
+        }
+    }
 }
--- SVRA/Assets/Scripts/SVRA/SampleInteractions/SVRA_MaterialToggle.cs
+++ SVRA/Assets/Scripts/SVRA/SampleInteractions/SVRA_MaterialToggle.cs
@@ -4,25 +4,70 @@ using UnityEngine;
 
 public class SVRA_MaterialToggle : MonoBehaviour
 {   
-    private bool toggle = false;
+    [Tooltip("Make the change of material permanent")]
+    public bool permanentChange = false;
+
+    [Tooltip("The list of materials to change between")]
+    public Material[] materialList;
+    
+    private bool singleMaterialToggle = true;
     private Material originalMaterial;
+    private Queue materialQueue = new Queue();
 
-    [Tooltip("The toggle material")]
-    public Material toggleMaterial;
-
-    // TODO : Add the ability to cycle through materials (material queue)
-    // public Material[] test;
-
-    void Start()
+    private void Awake()
     {
         originalMaterial = GetComponent<Renderer>().material;
-        ToggleMaterial();
+
+        for (int i = 0; i < materialList.Length; i++)
+        {
+            materialQueue.Enqueue(materialList[i]);
+        }
     }
-   
-    public void ToggleMaterial()
+
+    /* Change the material of the attached to object */
+    public void ChangeMaterial()
     {
-        GetComponent<Renderer>().material = (toggle ? toggleMaterial : originalMaterial);
-        toggle = !toggle;
+        if (materialList.Length == 1)
+        {
+            SingleMaterialChange();
+        }
+        else
+        {
+            MaterialQueueChange();
+        }
+    }
+
+    /* Toggle between the original and specified material */
+    void SingleMaterialChange()
+    {       
+        if (permanentChange)
+        {
+            GetComponent<Renderer>().material = materialList[0];
+        }
+        else
+        {
+            GetComponent<Renderer>().material = (singleMaterialToggle ? materialList[0] : originalMaterial);
+            singleMaterialToggle = !singleMaterialToggle;
+        }
+    }
+
+    /* Cycle through a queue of colors back to the original material */
+    void MaterialQueueChange()
+    {
+        if (permanentChange)
+        {
+            if (materialQueue.Count > 0)
+            {
+                GetComponent<Renderer>().material = (Material) materialQueue.Dequeue();
+            }
+
+            return;
+        }
+        else
+        {
+            materialQueue.Enqueue(GetComponent<Renderer>().material);
+            GetComponent<Renderer>().material = (Material) materialQueue.Dequeue();
+        }
     }
 
 }
